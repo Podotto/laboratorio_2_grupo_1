d4f0350e1a3f25389f13ae63f6d53aed9e4feb86
Podotto <grethelhalphen@gmail.com>
2026-02-21 15:05:00 -0500

    Correción del menú de opciones. Desarrollo del primer caso (opción hamburguesa).

--- 04_food_menu.py
+++ 04_food_menu.py
@@ -5,15 +5,36 @@ def main():
   print("======================================")
   
   #Menú de Opciones
-  print("1. Hamburguesa de Carne")
-  print("2. Hamburguesa de Pollo")
-  print("3. Nuggets")
-  print("4. Papas fritas")
-  print("5. Soda")
-  print("6. Sundae")
+  print("1. Hamburguesa")
+  print("2. Nuggets")
+  print("3. Papas fritas")
+  print("4. Soda")
+  print("5. Sundae")
   
   # Selección del usuario
-  option = int(input("¿Qué le gustaría añadir a su orden?: "))
+  option = int(input("¿Qué te gustaría añadir a tu orden?: "))
+  
+  # Casos
+  match option:
+    
+    # Caso 1
+    case 1:
+      print("¿Qué tipo de hamburguesa quieres?: ")
+      print("1. Hamburguesa de carne")
+      print("2. Hamburguesa de pollo")
+      
+      extra_burger = int(input("Escoje una opción: "))
+      
+      match extra_burger:
+        case 1: 
+          print("Añadiste una hamburguesa de carne.")
+        
+        case 2:
+          print("Añadiste una hamburguesa de pollo.")
+          
+        case _:
+          print("No es una opción válida.")
+      
   
 
 main()
