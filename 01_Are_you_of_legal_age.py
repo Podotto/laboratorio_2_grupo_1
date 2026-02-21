2decc65799dcabf3e1855420cd2eb9daa4a56edd
Adolfo-Noel <noelavila30@gmail.com>
2026-02-21 15:24:13 -0500

    proyecto de mayor o menor edad completado

---
+++ 01_Are_you_of_legal_age.py
@@ -0,0 +1,12 @@
+def main():
+    age = int(input("ingrese su edad:"))
+    
+    if age>=18:
+     print("Eres un adulto felicidades eres mayor de edad")
+     
+    else:
+      print("Eres un niño lamentable eres menor de edad")   
+main()     
+    
+    
+    
