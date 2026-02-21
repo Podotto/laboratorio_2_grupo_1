088c84e1d519f758fecebde7bf120b46f93b41a0
Podotto <grethelhalphen@gmail.com>
2026-02-21 16:23:43 -0500

    Se agrega bucle while para poder terminar el programa solo cuando el usuario decida terminar su orden.

--- 04_food_menu.py
+++ 04_food_menu.py
@@ -10,31 +10,40 @@ def main():
   print("3. Papas fritas")
   print("4. Soda")
   print("5. Sundae")
+  print("6. Terminar")
   
-  # Selección del usuario
-  option = int(input("¿Qué te gustaría añadir a tu orden?: "))
+  #Bucle
+  option = 0
+  while option != 6:
   
-  # Casos
-  match option:
+    # Selección del usuario
+    option = int(input("¿Qué te gustaría añadir a tu orden?: "))
     
-    # Caso 1
-    case 1:
-      print("¿Qué tipo de hamburguesa quieres?: ")
-      print("1. Hamburguesa de carne")
-      print("2. Hamburguesa de pollo")
+    # Casos
+    match option:
       
-      extra_burger = int(input("Escoje una opción: "))
-      
-      match extra_burger:
-        case 1: 
-          print("Añadiste una hamburguesa de carne.")
+      # Caso 1
+      case 1:
+        print("¿Qué tipo de hamburguesa quieres?: ")
+        print("1. Hamburguesa de carne")
+        print("2. Hamburguesa de pollo")
         
-        case 2:
-          print("Añadiste una hamburguesa de pollo.")
+        burger_type = int(input("Escoje una opción: "))
+        
+        match burger_type:
+          case 1: 
+            print("Añadiste una hamburguesa de carne.")
           
-        case _:
-          print("No es una opción válida.")
+          case 2:
+            print("Añadiste una hamburguesa de pollo.")
+            
+          case _:
+            print("No es una opción válida.")
       
-  
+      
+      
+      #Caso 6
+      case 6:
+        print("Hemos tomado tu orden. Por favor pasa a ventanilla para pagar.")  
 
 main()
