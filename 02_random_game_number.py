88e48c23525a32b75798398649dc4e7b5094ead1
Podotto <grethelhalphen@gmail.com>
2026-02-26 15:43:25 -0500

    Corrección de errores en la escritura del código.

--- 02_random_game_number.py
+++ 02_random_game_number.py
@@ -10,13 +10,13 @@ def main():
     print("He pensado un número entre 1 y 100. Intenta adivinarlo.")
 
     while not guess:
-        attempts = int(input("Ingresa tu número: "))
+        number = int(input("Ingresa tu número: "))
         attempts += 1
 
-        if attempts == number_secret:
+        if number == number_secret:
             print(f"¡Correcto! El número era {number_secret}. Lo lograste en {attempts} intentos.")
-            adivinado = True
-        elif attempts < number_secret:
+            guess = True
+        elif number < number_secret:
             print("El número secreto es mayor. Intenta otra vez.")
         else:
             print("El número secreto es menor. Intenta otra vez.")
