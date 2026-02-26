08e5b2404203da0d7cf316519eae2322ed7a163b
Adolfo-Noel <noelavila30@gmail.com>
2026-02-25 20:45:16 -0500

    Arreglo del idioma del proyecto

--- 02_random_game_number.py
+++ 02_random_game_number.py
@@ -2,21 +2,21 @@ import random
 
 def main():
    
-    numero_secreto = random.randint(1, 100)
-    intentos = 0
-    adivinado = False
+    number_secret = random.randint(1, 100)
+    attempts = 0
+    guess = False
 
     print("¡Bienvenido al juego de adivinar el número!")
     print("He pensado un número entre 1 y 100. Intenta adivinarlo.")
 
-    while not adivinado:
-        intento = int(input("Ingresa tu número: "))
-        intentos += 1
+    while not guess:
+        attempts = int(input("Ingresa tu número: "))
+        attempts += 1
 
-        if intento == numero_secreto:
-            print(f"¡Correcto! El número era {numero_secreto}. Lo lograste en {intentos} intentos.")
+        if attempts == number_secret:
+            print(f"¡Correcto! El número era {number_secret}. Lo lograste en {attempts} intentos.")
             adivinado = True
-        elif intento < numero_secreto:
+        elif attempts < number_secret:
             print("El número secreto es mayor. Intenta otra vez.")
         else:
             print("El número secreto es menor. Intenta otra vez.")
