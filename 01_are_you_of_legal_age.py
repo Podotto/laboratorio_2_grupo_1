fbc5398b33611dfbabf404dd66f228f8789e5c52
Juan Solis <juandsf.70@gmail.com>
2026-02-26 13:44:19 -0500

    Implementación de título y loop

--- 01_are_you_of_legal_age.py
+++ 01_are_you_of_legal_age.py
@@ -1,15 +1,17 @@
 def main():
+    print("===¿Eres mayor de edad?===")
     age = int(input("Ingrese su edad: "))
-    
-    if age>=18:
-     print("Eres un adulto. ¡Felicidades! Eres mayor de edad")
-     
+
+    if age >= 18:
+        print("Eres un adulto. ¡Felicidades! Eres mayor de edad")
+
     elif age < 0:
-      print("Error. Edad no válida.")
-     
+        print("Error. Edad no válida.")
+
     else:
-      print("Eres un niño. Lamentable eres menor de edad")   
-main()     
-    
-    
-    
+        print("Eres un niño. Lamentable eres menor de edad")
+
+
+if __name__ == "__main__":
+    while True:
+        main()
